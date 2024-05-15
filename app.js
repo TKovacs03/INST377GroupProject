a53ab6e652c637a30ad39ffe510291d14499410b
Thomas Kovacs <tkovacs@terpmail.umd.edu>
2024-05-14 21:04:02 -0400

    reformatting for vercel test

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const supabaseURL = 'https://prwypsbxgfzqwqltvszm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByd3lwc2J4Z2Z6cXdxbHR2c3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3MjM1ODQsImV4cCI6MjAzMTI5OTU4NH0.zO5Egnl8RKYhn-YzEd3m5qsZh_Udgk2VuDdZQUSTfkY'
 const supabase = supabaseClient.createClient(supabaseURL,supabaseKey)
 
+app.get('/', (req, res) => {
+    res.sendFile('public/mainpage.html', {root: __dirname})
+})
+
 app.get('/test', async (req, res) => {
     console.log('Hooray!')
 
